8945f4e8558e3061846c6496624a4b7241bfe40e
Alex <alex@MacBook-Air-Alex.local>
2022-10-28 13:04:04 +0600

    Added finished commit

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import Routing from './Routing';
 import AuthContextProvider from './contexts/authContext';
+import ProductsContextProvider from './contexts/productContext';
 
 const App = () => {
   return (
-    <AuthContextProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Routing />
-      </BrowserRouter>
-    </AuthContextProvider>
+    <ProductsContextProvider>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Routing />
+        </BrowserRouter>
+      </AuthContextProvider>
+    </ProductsContextProvider>
   )
 }
 
--- src/components/products/ProductCard/ProductCard.jsx
+++ src/components/products/ProductCard/ProductCard.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { productsContext } from '../../../contexts/productContext';
 
-const ProductCard = () => {
+const ProductCard = ({ item }) => {
+  const { deleteProduct, toggleLike } = useContext(productsContext);
+
   return (
-    <div>ProductCard</div>
+    <div>
+      Title: {item.title}
+      Price: ${item.price}
+      Category: {item.category.title}
+      Reviews: {item.reviews.length}
+      Like: {item.likes}
+      <button>Details</button>
+      <button onClick={() => toggleLike(item.id)}>Like</button>
+      {item.is_author ? (
+        <>
+          <button>Edit</button>
+          <button onClick={() => deleteProduct(item.id)}>Delete</button>
+        </>
+      ) : (
+        null
+      )}
+    </div>
   )
 }
 
